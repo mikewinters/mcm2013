376f68d1a68d2f6af9706952a359d4b4bb31aa8f
mikewinters <Raymond.Winters@mail.mcgill.ca>
2012-11-27 17:24:33 -0500

    A few additions by Jon

--- sidebar.php
+++ sidebar.php
@@ -1,12 +1,16 @@
 <ul>
     <li>
     <h2>A Common Ground</h2>
-    <p>The disciplines of Mathematics and Music share an intertwined history stretching back more than two and a half millennia. More recently, informatics has made possible new approaches to these disciplines, often with transformative effect. MCM conferences promote the collaboration and exchange of ideas among researchers in music theory, mathematics, computer science, musicology, cognition, and other related fields.</p>
+    <p>The disciplines of Mathematics and Music share an intertwined history stretching back more than two and a half 
+millennia. More recently, informatics has made possible new approaches to these disciplines, often with transformative 
+effect. International MCM conferences, held biennially, continue to promote the collaboration and exchange of ideas among 
+researchers in music theory, mathematics, computer science, musicology, cognition, and other related fields, to 
+further our understanding of a wide range of musical phenomena.</p>
     
     <h2>Calendar</h2>
-    <p>Submission: Jan. 15<br>
-        Decision: Feb. 20<br>
-        Final Version: Apr. 1<br>
+    <p><a href="submission.php">Submission</a>: Jan. 15<br>
+        Decisions communicated by: Feb. 20<br>
+        Final version due: Apr. 1<br>
         Conference: June 12-14<br></p>
     </li>
 </ul>
